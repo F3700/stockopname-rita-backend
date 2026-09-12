1bfa8a8bd00be68559ebe68ffb1f190fd357ad57
F3700 <albertfebrian26@gmail.com>
2026-09-12 10:07:51 +0700

    fix: allow environment variables without env file

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -16,9 +16,8 @@ import (
 )
 
 func main() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
+	if err := godotenv.Load(); err != nil {
+		log.Println("No .env file found, using environment variables")
 	}
 
 	host := os.Getenv("APP_HOST")
